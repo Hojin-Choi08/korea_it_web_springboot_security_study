86be33592488478053b2d8c1a6a64f9827f87fb1
Hojin Choi <merro3043@gmail.com>
2025-06-30 19:38:31 +0900

    Feat: commit

--- src/main/java/com/koreait/SpringSecurityStudy/controller/AuthController.java
+++ src/main/java/com/koreait/SpringSecurityStudy/controller/AuthController.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
